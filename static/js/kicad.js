129731db1e13527346bed0ba8c391101fa0a7368
Mark Roszko <mark.roszko@gmail.com>
2015-08-23 18:05:57 -0400

    Make the footer a solid color, hackishy make the header static for mobile. Need to redo how that all functions because between the responsive css and JS, its just awful.

--- static/js/kicad.js
+++ static/js/kicad.js
@@ -7,12 +7,28 @@
 if( $('.navbar').hasClass('transparent') )
 {
 	$(window).scroll(function() {
-
 		if($(this).scrollTop() < 430) { 
-			$('.navbar').addClass('transparent'); // adding the opaque class
+			$('.navbar').addClass('transparent');
 		} else {
-			$('.navbar').removeClass('transparent'); // removing the opaque class
+			$('.navbar').removeClass('transparent');
 		}
 	});
 }
+
+	$(window).resize(function() {
+		var mobileLimit = 760;
+		if ($(window).width() < mobileLimit) {
+			$('.navbar').addClass('navbar-static-top');
+			$('.navbar').removeClass('navbar-fixed-top');
+			$('header').addClass('page-header-decor-no-margin');
+		}
+		else 
+		{
+			$('.navbar').removeClass('navbar-static-top');
+			$('.navbar').addClass('navbar-fixed-top');
+			$('header').removeClass('page-header-decor-no-margin');
+		}
+	});
+	$(window).trigger('resize');
+
 })();
